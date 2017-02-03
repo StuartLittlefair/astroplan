fdeedf520bc8143fd839567933e0a1e8b2f1aa02
Stuart Littlefair <s.littlefair@shef.ac.uk>
2017-02-03 20:05:17 +0000

    minor fix to handle no targets

--- astroplan/observer.py
+++ astroplan/observer.py
@@ -359,7 +359,7 @@ class Observer(object):
                 return False
         return True
 
-    def _preprocess_inputs(self, time, target, grid=False):
+    def _preprocess_inputs(self, time, target=None, grid=False):
         """
         Preprocess time and target inputs
 
